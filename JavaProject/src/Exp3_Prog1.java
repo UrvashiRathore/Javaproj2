31bfdb662705859e497fc1ca2b5b4f45163b2961
UrvashiRathore <rathore.urvashi21@gmail.com>
2021-09-15 06:56:57 +0530

    commit lab 3 exp.

---
+++ JavaProject/src/Exp3_Prog1.java
@@ -0,0 +1,16 @@
+
+public class Exp3_Prog1 {
+
+	public static void main(String[] args) {
+		for (int i =1 ; i<10 ; i++) {
+			for(int j = 0 ; j<10;j++) {
+				for (int k =0;k<10;k++) {
+					int sum = i*100 + j*10 + k ;
+					System.out.print("\n number is : "+sum);
+     
+				}
+			}
+		}
+	}
+
+}

3502206d85016f7c59db5bb622db66abfbe3b2b2
UrvashiRathore <rathore.urvashi21@gmail.com>
2021-10-17 20:33:50 +0530

    edit

--- JavaProject/src/Exp4_Prog1.java
+++ JavaProject/src/Exp4_Prog1.java
@@ -14,4 +14,15 @@ class DailyWorker extends Worker
 	salary = day*spd;
 	return salary;
 	}
+	DailyWorker()
+	{
+		System.out.println("Dailyworker object");
+	}
+	public class Exp4_Prog1{
+		public static void main(String[] args) {
+			Worker w;
+			w = new DailyWorker();
+			w.calculateSalary();
+		}
+}
 }

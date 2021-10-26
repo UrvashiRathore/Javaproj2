5e750d49577f2ff4c2be5b0865e16bc03f381f08
UrvashiRathore <rathore.urvashi21@gmail.com>
2021-10-26 19:31:58 +0530

    j

---
+++ JavaProject/src/Exp6_Prog1.java
@@ -0,0 +1,15 @@
+public class Exp6_Prog1 {
+	public static void main(String[] args)
+	{
+			 try {
+				 
+		            int a[] = { 1, 2, 3, 4, 5 };
+		  
+		            System.out.println(a[7]);
+		}
+			 catch (Exception e1) {
+		            System.out.println("ArrayIndexOutOfBoundsException");
+		        }
+		
+}
+}
